e93aa1194eab6b4640373d560b02c7b969ef5681
Akshay-gundeti <104581998+Akshay-gundeti@users.noreply.github.com>
2024-07-24 12:35:27 +0530

    Add files via upload

---
+++ CombineStrings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CombineStrings {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        String s2=s.nextLine();
+        System.out.println(s1.concat(s2));
+
+
+    }
+}
